5fdf2ce0bcaae77c35929c182620465c65e4e342
fawda123 <mbafs2012@gmail.com>
2018-01-24 16:25:50 -0800

    found station lat/lon for Bight 08

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 
+# initial data processing
 dat <- read_excel('ignore/Sed_chem_tox.xlsx') %>% 
   .[-1, ] %>% 
   select(-`Amphipod survival (n =20)`, -`X__2`) %>% 
@@ -15,7 +16,20 @@ dat <- read_excel('ignore/Sed_chem_tox.xlsx') %>%
   mutate(
     fine = as.numeric(fine), 
     amph_surv = as.numeric(amph_surv),
-    embr_surv = as.numeric(embr_surv)
+    embr_surv = as.numeric(embr_surv),
+    station = gsub("B08-", '', station)
+  )
+
+# add station lat/lon
+locs <- read.csv('ignore/AllBightStationLocations.csv', stringsAsFactors = F) %>% 
+  filter(Bight %in% 2008) %>% 
+  rename(station = StationID) %>% 
+  select(station, Latitude, Longitude)
+dat <- dat %>% 
+  left_join(locs, by = 'station') %>% 
+  rename(
+    lat = Latitude, 
+    lon = Longitude
   )
 
 save(dat, file = 'data/dat.RData', compress = 'xz')
