c52eb4f43f66164775d16fa109871d4af01155fc
fawda123 <mbafs2012@gmail.com>
2018-02-13 09:07:40 -0800

    added complete data with metals, not analyzed yet

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,29 +2,27 @@ library(tidyverse)
 library(readxl)
 
 # initial data processing
-dat <- read_excel('ignore/Sed_chem_tox.xlsx') %>% 
+dat <- read_excel('ignore/Sed_chem_tox - expanded.xlsx') %>% 
   .[-1, ] %>% 
   select(-`Amphipod survival (n =20)`, -`X__2`) %>% 
   rename(
-    station = `Station Identity`, 
+    station = `Station ID`, 
+    depth = `Depth\r\n(m)`,
     toc = `%TOC`,
     tn = `%TN`,
     fine = `%Fine`,
     amph_surv = X__1, 
     embr_surv = `%M.Emb.Surv.`
   ) %>% 
-  mutate(
-    fine = as.numeric(fine), 
-    amph_surv = as.numeric(amph_surv),
-    embr_surv = as.numeric(embr_surv),
-    station = gsub("B08-", '', station)
-  )
+  mutate_all(function(x){ x[x %in% '?'] <- NA; return(x)} ) %>% 
+  mutate_if(is.character, as.numeric)
 
 # add station lat/lon
 locs <- read.csv('ignore/AllBightStationLocations.csv', stringsAsFactors = F) %>% 
   filter(Bight %in% 2008) %>% 
   rename(station = StationID) %>% 
-  select(station, Latitude, Longitude)
+  select(station, Latitude, Longitude) %>% 
+  mutate(station = as.numeric(station))
 dat <- dat %>% 
   left_join(locs, by = 'station') %>% 
   rename(
