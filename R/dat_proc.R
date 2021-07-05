7fe8d39d391174d60a31b5dfd5f462de5807ae3e
fawda123 <mbafs2012@gmail.com>
2021-07-05 17:12:42 -0400

    ignore to data-raw

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(readxl)
+library(here)
 
 # initial data processing
-dat <- read_excel('ignore/Sed_chem_tox - expanded.xlsx') %>% 
+dat <- read_excel(here('data-raw/Sed_chem_tox - expanded.xlsx')) %>% 
   .[-1, ] %>% 
   select(-`Amphipod survival (n =20)`, -`X__2`) %>% 
   rename(
@@ -18,7 +19,7 @@ dat <- read_excel('ignore/Sed_chem_tox - expanded.xlsx') %>%
   mutate_if(is.character, as.numeric)
 
 # add station lat/lon
-locs <- read.csv('ignore/AllBightStationLocations.csv', stringsAsFactors = F) %>% 
+locs <- read.csv(here('data-raw/AllBightStationLocations.csv'), stringsAsFactors = F) %>% 
   filter(Bight %in% 2008) %>% 
   rename(station = StationID) %>% 
   select(station, Latitude, Longitude) %>% 
@@ -30,4 +31,4 @@ dat <- dat %>%
     lon = Longitude
   )
 
-save(dat, file = 'data/dat.RData', compress = 'xz')
+save(dat, file = here('data/dat.RData'), compress = 'xz')
